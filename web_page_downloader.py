3c57a83971dbdf5db37087134ba9d24160bce380
igwebuikedominic@gmail.com <igwebuikedominic@gmail.com>
2026-05-26 00:33:17 +0100

    feat: bypass 403 server rejections and download file

--- web_page_downloader.py
+++ web_page_downloader.py
@@ -6,7 +6,15 @@ def web_saver(url:str, filename:str) -> str:
     if not filename.endswith(".html"):
         filename += ".html"
         
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Cache-Control": "max-age=0"
+    }
 
     try:
         # using timeout 
@@ -29,10 +37,11 @@ def web_saver(url:str, filename:str) -> str:
 
 if __name__ == "__main__":
 
-    target_url = input("Enter Link Here: ".upper())
+    url = input("Enter Link Here: ".upper())
+    clean_url = url.strip().encode('ascii', 'ignore').decode('ascii')
     save_as = input("Enter the name you want the file to be saved as: ".upper())
 
-    result = web_saver(target_url, save_as)
+    result = web_saver(clean_url, save_as)
 
     print(result)
     
